b95e8b9207f5ac88b7ecba271d36d0d3887321c7
hmemon99 <hajimemon@Haji-Memons-MacBook-Pro.local>
2014-08-26 20:10:08 -0400

    Fizz Buzz Refactored
    Refactored with input form

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,30 +1,24 @@
 $(document).ready(function(){
-	$("button").on('click',fizzBuzz);
-})
-function fizzBuzz() {
-	for (var i = 1; i < 101; i++){
-		if (i % 3 == 0 && i % 5 == 0) {
-			console.log ('FizzBuzz');
-			$(".numbers-here").append('<p>FizzBuzz</p>')
-		}
-		else if (i % 5 == 0) {
-			console.log('Buzz');
-			$(".numbers-here").append('<p>Buzz</p>')
-		}
-		else if (i % 3 == 0) {
-			console.log('Fizz');
-			$(".numbers-here").append('<p>Fizz</p>')	
-		}
-		else {
-			console.log(i);
-			$(".numbers-here").append('<p>' +i+ '</p>')
-		}
-	}	
-}
-
-	
-//need something that limits the number to 100
-//need append functionality that displays the numbers preceding the typed number below
-//need functionality for fizz
-//need functionality for buzz
-//need functionality for fizzbuzz
+	$("button").on('click', function(){
+		var number = $("#user-number").val();
+		for (var i = number; i < 101; i++){
+			if (i % 3 == 0 && i % 5 == 0) {
+				console.log('FizzBuzz');
+				$(".numbers-here").append('<p>FizzBuzz</p>')
+			}
+			else if (i % 5 == 0) {
+				console.log('Buzz');
+				$(".numbers-here").append('<p>Buzz</p>')
+			}
+			else if (i % 3 == 0) {
+				console.log('Fizz');
+				$(".numbers-here").append('<p>Fizz</p>')	
+			}
+			else {
+				console.log(i);
+				$(".numbers-here").append('<p>' +i+ '</p>')
+			}
+		}	
+			return false;	
+	});
+});
